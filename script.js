7f8be5b35a18324367db3c771fc1b3755d3deee0
Nadia Putri <nadzdea.25@gmail.com>
2018-02-11 20:27:43 +0800

    Move Javascript script to bottom of HTML to run it when DOM is ready

--- script.js
+++ script.js
@@ -1,16 +1,2 @@
 console.log("Run javascript file");
-
-for (var i=0; i<16; i++) {
-    for (var j=0; j<16; j++) {
-        var div = document.createElement("div");
-        div.style.width = "100px";
-        div.style.height = "100px";
-        div.style.background = "#666666";
-        div.style.border = "#000";
-
-        var jump = document.createElement("br");
-        
-        document.getElementById("container").appendChild(jump);
-        document.getElementById("container").appendChild(div);
-    }
-}
+console.log(document.getElementById("container") == null);
