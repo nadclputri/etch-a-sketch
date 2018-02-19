14bbd7aee00a31e4ba99debb7609d8ec3093e786
Nadia Putri <nadzdea.25@gmail.com>
2018-02-19 13:48:16 +0800

    Restructure javascript function to make it neater

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-// Create 16x16 grid
+function newGrid(num) {
+    for (var i=0; i<num; i++) {
+        for (var j=0; j<num; j++) {
+            let div = document.createElement("div");
+            div.style.background = "#666666";
+            div.style.border = "#000";
+            div.className = "grid";
 
-for (var i=0; i<16; i++) {
-    for (var j=0; j<16; j++) {
-        let div = document.createElement("div");
-        div.style.background = "#666666";
-        div.style.border = "#000";
-        div.className = "grid";
+            document.getElementById("container").appendChild(div);
+        }
 
-        document.getElementById("container").appendChild(div);
-    }   
+        let jump = document.createElement("br");
+        document.getElementById("container").appendChild(jump);
+    }
 
-    let jump = document.createElement("br");
-    document.getElementById("container").appendChild(jump);
+    let boxes = document.querySelectorAll(".grid");
+    boxes.forEach(square => {
+        square.addEventListener("mouseover", function(event) {
+            event.target.style.background = "blue";
+        });
+    });
 }
 
-// Add hover effect to grid
-let boxes = document.querySelectorAll(".grid");
-boxes.forEach(square => {
-    square.addEventListener("mouseover", function(event) {
-        event.target.style.background = "blue";
-    });
-});
+newGrid(16);
