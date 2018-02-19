8f97f1b4a87605499611392d9b5ca2ea59272a09
Nadia Putri <nadzdea.25@gmail.com>
2018-02-19 09:26:07 +0800

    Create 16x16 grid

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-console.log("Run javascript file");
-console.log(document.getElementById("container") == null);
+for (var i=0; i<16; i++) {
+    for (var j=0; j<16; j++) {
+        var div = document.createElement("div");
+        div.style.width = "50px";
+        div.style.height = "50px";
+        div.style.background = "#666666";
+        div.style.border = "#000";
+        div.className = "box";
+        
+        document.getElementById("container").appendChild(div);
+    }   
+
+    var jump = document.createElement("br");
+    document.getElementById("container").appendChild(jump);
+}
