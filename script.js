50e1fe75f827359b5b9f54aae1b05784ce4f7e4e
Nadia Putri <nadzdea.25@gmail.com>
2018-02-19 13:23:48 +0800

    Add hover function to the grid

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
+// Create 16x16 grid
+
 for (var i=0; i<16; i++) {
     for (var j=0; j<16; j++) {
-        var div = document.createElement("div");
+        let div = document.createElement("div");
         div.style.background = "#666666";
         div.style.border = "#000";
-        div.className = "box";
-        
+        div.className = "grid";
+
         document.getElementById("container").appendChild(div);
     }   
 
-    var jump = document.createElement("br");
+    let jump = document.createElement("br");
     document.getElementById("container").appendChild(jump);
 }
+
+// Add hover effect to grid
+let boxes = document.querySelectorAll(".grid");
+boxes.forEach(square => {
+    square.addEventListener("mouseover", function(event) {
+        event.target.style.background = "blue";
+    });
+});
